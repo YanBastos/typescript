190586df17979fdecd5f8970c90068681fb981f4
YanBastos <yanmoraesbastos@hotmail.com>
2024-02-10 13:04:00 -0300

    ...

--- typescript_exercise/ex_criando_tipos/src/index.ts
+++ typescript_exercise/ex_criando_tipos/src/index.ts
@@ -31,8 +31,9 @@ function findPlanet(name: string) {
 function updateSituation(situation: PlanetSituation, planet: Planet) {
     planet.situation = situation
 
-    alert(`A situação do planeta ${planet.name} foi atualizada para ${situation}`)
+    alert(`A situação do planeta ${planet.name} foi atualizada para "${situation}".`)
 }
+
 
 function addSatellite(name: string, planet: Planet) {
     planet.satellites.push(name)
@@ -44,4 +45,36 @@ function removeSatellite(name: string, planet: Planet) {
     planet.satellites = planet.satellites.filter(satellite => satellite !== name)
 
     alert(`O satellite ${name} foi removido do planeta ${planet.name}`)
+}
+
+
+function promptValidSituation() {
+    let situation: PlanetSituation
+    let validSituation = false
+
+    while (!validSituation) {
+        const situationInput = prompt('Informe a situação do planeta?\n1 - Habitado\n2 - Habitável\n3 - Inabitável\n4 - Inexplorado')
+
+        switch (situationInput) {
+            case '1':
+                situation = 'Habitado'
+                validSituation = true
+                break;
+            case '2':
+                situation = 'Habitavel'
+                validSituation = true
+                break;
+            case '3':
+                situation = 'Inabitavel'
+                validSituation = true
+                break;
+            case '4':
+                situation = 'Inexplorado'
+                validSituation = true
+                break;
+            default:
+                alert('Situação inválida!')
+                break;
+        }
+    }
 }
