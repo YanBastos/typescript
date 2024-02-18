10a8fe77d99dcef944978f4856efd7bf52edcee2
YanBastos <yanmoraesbastos@hotmail.com>
2024-02-17 21:25:20 -0300

    ...

--- criando-interface/src/index.ts
+++ criando-interface/src/index.ts
@@ -28,7 +28,7 @@ let sun: Star = {
 
 // classes podem implementar interfaces
 // nesse caso ela cria o que chamamos de contrato,
-//pois obriga a classe a implementar tudo o que
+// pois obriga a classe a implementar tudo o que
 // foi definido na interface
 class MilkWayPlanet implements Planet {
     // em typescript precisamos explicitar os atributos da classe dessa forma
@@ -42,5 +42,42 @@ class MilkWayPlanet implements Planet {
         this.population = population
     }
     createrSatellite!: (name: string) => void
+}
+
+// Aliases também podem usar heranças mas co uma 
+// sintaxe um pouco diferente
+type Asteroid = CelestialBody & {
+    size: number
+}
+
+// O mesmo é válido para implementação usando Type de objeto
+class BigAsteroid implements Asteroid {
+    name: string
+    mass: number
+    size: number
+
+    constructor(n: string, m: number, s: number) {
+        this.name = n
+        this.mass = m
+        this.size = s
+    }
+}
+
+// Apesar de serem poucas, existem diferenças entre type e interfaces
+
+// Uma delas é a possibilidade de adicionar mais
+// propriedades definindo novamente a mesma interface
+interface Planet {
+    orbitedStar: Star
+}
+
+let earth: Planet = {
+    name: "Terra",
+    mass: 5.972 * Math.pow(10, 24),
+    population: 8000000000,
+    createrSatellite: (name: string) => {
+        //...
+    },
+    orbitedStar: sun
 }
 
